2ced5a40fa55af0556d006b6b9b98ee4e50a5417
aakture <alper.akture@gmail.com>
2015-09-18 12:47:33 -0700

    use ucp pool

--- src/main/java/com/dao/ConnectionPool.java
+++ src/main/java/com/dao/ConnectionPool.java
@@ -2,6 +2,8 @@ package com.dao;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,46 +18,46 @@ public enum ConnectionPool {
     INSTANCE;
 
     private final Logger log = LoggerFactory.getLogger(ConnectionPool.class);
-    private HikariDataSource ds;
+//    private HikariDataSource ds;
+    private PoolDataSource pds;
 
     private ConnectionPool() {
-        log.info("Construction of ConnectionPool");
-        String jdbcUrl = "jdbc:mysql://localhost:3306/test?useServerPrepStmts=true&useAffectedRows=true";
-        HikariConfig config = new HikariConfig();
-        config.setMaximumPoolSize(2);
-        config.setConnectionTimeout(5500);
-        config.setDataSourceClassName("com.mysql.jdbc.jdbc2.optional.MysqlDataSource");
-        config.addDataSourceProperty("url", jdbcUrl);
-        config.addDataSourceProperty("user", "root");
-        config.addDataSourceProperty("password", "");
+        try {
+            log.info("Construction of ConnectionPool");
+            String jdbcUrl = "jdbc:mysql://localhost:3306/test?useServerPrepStmts=true&useAffectedRows=true";
+//        HikariConfig config = new HikariConfig();
+//        config.setMaximumPoolSize(2);
+//        config.setConnectionTimeout(5500);
+//        config.setDataSourceClassName("com.mysql.jdbc.jdbc2.optional.MysqlDataSource");
+//        config.addDataSourceProperty("url", jdbcUrl);
+//        config.addDataSourceProperty("user", "root");
+//        config.addDataSourceProperty("password", "");
+//
+//        config.addDataSourceProperty("cachePrepStmts", true);
+//        config.addDataSourceProperty("prepStmtCacheSize", 250);
+//        config.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
+//        config.addDataSourceProperty("autoReconnect", true);
+//        config.setConnectionTestQuery("SELECT 1");
+//        log.info("initializing database connection pool to: {}. If this takes a long time, check that the URL, user, and password are all correct.", jdbcUrl);
+//        ds = new HikariDataSource(config);
+            pds = PoolDataSourceFactory.getPoolDataSource();
+            pds.setConnectionFactoryClassName("com.mysql.jdbc.jdbc2.optional.MysqlDataSource");
+            pds.setURL("jdbc:mysql://localhost:3306/test?useServerPrepStmts=true&useAffectedRows=true");
+            pds.setUser("root");
+            pds.setPassword("");
 
-        config.addDataSourceProperty("cachePrepStmts", true);
-        config.addDataSourceProperty("prepStmtCacheSize", 250);
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
-        config.addDataSourceProperty("autoReconnect", true);
-        config.setConnectionTestQuery("SELECT 1");
-        log.info("initializing database connection pool to: {}. If this takes a long time, check that the URL, user, and password are all correct.", jdbcUrl);
-        ds = new HikariDataSource(config);
-        log.info("initializing database connection pool complete");
+            log.info("initializing database connection pool complete");
+        }catch(Exception ex) {
+            ex.printStackTrace();
+            log.error("error", ex);
+        }
 
     }
 
     public Connection getConnection() throws SQLException {
-        return ds.getConnection();
+        return pds.getConnection();
     }
 
-    /**
-     * Only call this when the app shuts down to close pool of if you need to
-     */
-    public void close() {
-        log.info("shutting down pool");
-        try {
-            if (ds != null) {
-                ds.close();
-            }
-        }catch(Exception ex) {
-            log.error("error closing pool", ex);
-        }
-    }
+
 
 }
